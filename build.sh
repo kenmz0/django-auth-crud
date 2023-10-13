a3f14de284a93b6c7af476ae87fd8a6113810968
kenmz0 <94247331+kenmz0@users.noreply.github.com>
2023-10-13 16:53:10 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py createsuperuser --username muniz --email admin@admin.com --no-input
+#python manage.py createsuperuser --username muniz --email admin@admin.com --no-input
 python manage.py collectstatic --no-input
 python manage.py migrate
